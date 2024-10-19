ec69df750de1b2fd33e64cb185b121f6c230c9d2
jy0830 <jyeop0830@gmail.com>
2024-10-19 09:15:59 +0900

    tt

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import Register from "./Register";
+import { UserContext } from "./UserContext";
+
+
+export default function Routes() {
+    const {username, id} = useContext(UserContext);
+
+
+    if (username) {
+        return 'Logged In!' + username;
+    }
+
+
+    return (
+        <Register />
+    )
+}
+
+
